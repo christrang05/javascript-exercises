2cf5242487d854134613ef975012f2a3a8e1cd50
christrang05 <133800895+christrang05@users.noreply.github.com>
2024-01-11 15:06:34 -0600

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let smallNum = Math.min(num1,num2);
+    let largeNum = Math.max(num1,num2);
+    let sum = 0
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || isNaN(num1) || typeof num2 !== "number" || isNaN(num2)){
+        return "ERROR";
+    }
+    for (let i = smallNum; i <= largeNum; i++) {
+        sum += i;
+    }
 
-};
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
