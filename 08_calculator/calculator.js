a595c1b56c3311e8174b3551d60bb30111b7fec4
christrang05 <133800895+christrang05@users.noreply.github.com>
2024-01-11 16:53:50 -0600

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(add1, add2) {
+	return add1 + add2;
 };
 
-const subtract = function() {
-	
+const subtract = function(sub1, sub2) {
+	return sub1 - sub2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(mulArray) {
+  return mulArray.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n ===0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
