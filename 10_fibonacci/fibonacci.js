29ac0919f8e1305e655cfdf8aa71179ce49ec70a
christrang05 <133800895+christrang05@users.noreply.github.com>
2024-01-25 15:35:18 -0600

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(sequenceNumber) {
+    const number = parseInt(sequenceNumber, 10);
+    if (number < 0) return "OOPS";
+    if (number === 0 || number === 1) return number;
 
+    let a = 0;
+    let b = 1;
+    let fib = 0;
+    for (let i = 2; i <= number; i++) {
+       fib = a + b;
+       a = b;
+       b = fib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
