daa56ef8aae247aa814c89fe526255c7c2c76b6f
christrang05 <133800895+christrang05@users.noreply.github.com>
2024-01-25 15:14:22 -0600

    09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    const normalizedString = string.replace(/[\W_]/g, '').toLowerCase();
+    const reversedString = normalizedString.split('').reverse().join('');
 
+    return normalizedString === reversedString;
 };
 
 // Do not edit below this line
